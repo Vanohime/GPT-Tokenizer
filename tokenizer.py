ad77450eb327ef8226a248f24e661bfadcb54870
Ivan Harohau <olevel806@gmail.com>
2025-02-01 23:18:02 +0300

    merge and get_stats fixed

--- tokenizer.py
+++ tokenizer.py
@@ -1,26 +1,22 @@
 import regex as re
 
-def get_stats(ids: [list[list[int]]]):
-    res = {}
-    for lst in ids:
-        for pair in zip(lst, lst[1:]):
-            res[pair] = res.get(pair, 0) + 1
-    return res
+def get_stats(ids, counts=None):
+    if counts is None:
+        counts = {}
+    for pair in zip(ids, ids[1:]):
+        counts[pair] = counts.get(pair, 0) + 1
+    return counts
 
-def merge(ids: list[list[int]], pair, ind: int):
+def merge(ids, pair, ind):
     newids = []
-    for lst in ids:
-        newlst = []
-        i = 0
-        while i < len(lst):
-            if i < len(lst) - 1 and pair[0] == lst[i] and pair[1] == lst[i+1]:
-                newlst.append(ind)
-                i += 2
-            else:
-                newlst.append(lst[i])
-                i += 1
-
-        newids.append(newlst)
+    i = 0
+    while i < len(ids):
+        if i < len(ids) - 1 and pair[0] == ids[i] and pair[1] == ids[i + 1]:
+            newids.append(ind)
+            i += 2
+        else:
+            newids.append(ids[i])
+            i += 1
     return newids
 
 
@@ -36,10 +32,13 @@ class RegexTokenizer:
         i = 0
         num_merges = vocab_size - 256
         for i in range(num_merges):
-            stats = get_stats(tokens)
+            stats = {}
+            for j in range(len(tokens)):
+                get_stats(tokens[j], stats)
             pair_to_merge = max(stats, key=stats.get)
             self.merges[pair_to_merge] = 256 + i
-            tokens = merge(tokens, pair_to_merge, 256 + i)
+            for j in range(len(tokens)):
+                tokens[j] = merge(tokens[j], pair_to_merge, 256 + i)
             print(f"merged pair {pair_to_merge} to token {256 + i}")
         self.create_vocab()
 
@@ -52,18 +51,25 @@ class RegexTokenizer:
     def encode(self, text):
         ids = list(map(int, text.encode("utf-8")))
         while len(ids) >=2:
-            stats = get_stats([ids])
+            stats = get_stats(ids)
             pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
             if pair not in self.merges:
                 break
             ind = self.merges[pair]
-            ids = merge([ids], pair, ind)
-        return ids[0]
+            ids = merge(ids, pair, ind)
+        return ids
 
 
     def decode(self, ids):
-        tokens = b"".join(self.vocab[idx] for idx in [lst for lst in ids])
+        tokens = b"".join(self.vocab[idx] for idx in ids)
         text = tokens.decode("utf-8", errors="replace")
         return text
 
 
+tok = RegexTokenizer()
+text = open("taylorswift.txt", 'r').read()
+print(len(list(set(text))))
+vocab_size = 30 + 256
+tok.train(text, vocab_size)
+test = "09 premiere of Hannah Montana: The Movie. She had a cameo appearance in the film and wrote two songs for its soundtrack.[71][72]Swift's second studio album, Fearless, was"
+print(tok.decode(tok.encode(test)) == test)
